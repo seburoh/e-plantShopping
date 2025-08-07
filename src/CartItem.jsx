8c6159be5fe76f9afb21fdfe1b238f3c79b919b0
seburoh <seburoh@outlook.com>
2025-08-07 12:07:45 -0700

    fixes

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -36,7 +36,11 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }))
+    if (item.quantity <= 1) {
+      handleRemove(item);
+    } else {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }))
+    }
   };
 
   const handleRemove = (item) => {
