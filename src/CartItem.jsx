70eac96e08bb4eb46b6cec10c1ba9c4ff7f24bdb
seburoh <seburoh@outlook.com>
2025-08-07 12:56:35 -0700

    prettyfy the cart add ghpages

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -19,6 +19,10 @@ const CartItem = ({ onContinueShopping }) => {
     return total;
   };
 
+  const totalItems = () => {
+    return cart ? cart.reduce((total, item) => total + item.quantity, 0) : 0;
+  };
+
   const handleContinueShopping = (e) => {
     onContinueShopping(e);
   };
@@ -54,14 +58,14 @@ const CartItem = ({ onContinueShopping }) => {
 
   return (
     <div className="cart-container">
-      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <h2 style={{ color: 'black' }}>Total Cart Amount for {totalItems()} plants: ${calculateTotalAmount()}</h2>
       <div>
         {cart.map(item => (
           <div className="cart-item" key={item.name}>
             <img className="cart-item-image" src={item.image} alt={item.name} />
             <div className="cart-item-details">
               <div className="cart-item-name">{item.name}</div>
-              <div className="cart-item-cost">{item.cost}</div>
+              <div className="cart-item-cost">${item.cost}</div>
               <div className="cart-item-quantity">
                 <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
